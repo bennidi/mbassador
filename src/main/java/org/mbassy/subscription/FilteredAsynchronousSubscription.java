2ae0e5fae09cb0cf4127a5528b21de0e7b2cf60d
benni <benjamin.diedrichsen@friendsurance.de>
2012-11-17 12:13:55 +0100

    introduced specialized subscriptions for better performance and customization options

---
+++ src/main/java/org/mbassy/subscription/FilteredAsynchronousSubscription.java
@@ -0,0 +1,33 @@
+package org.mbassy.subscription;
+
+import org.mbassy.IMessageBus;
+import org.mbassy.IPublicationErrorHandler;
+import org.mbassy.listener.MessageFilter;
+import org.mbassy.listener.MessageHandlerMetadata;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+/**
+* Created with IntelliJ IDEA.
+* User: benni
+* Date: 11/14/12
+* Time: 3:50 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class FilteredAsynchronousSubscription extends FilteredSubscription{
+
+    public FilteredAsynchronousSubscription(IMessageBus mBassador, MessageHandlerMetadata messageHandler) {
+        super(mBassador, messageHandler);
+    }
+
+    protected void dispatch(final Object message, final Object listener){
+
+        getMessageBus().getExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                invokeHandler(message, listener);
+            }
+        });
+    }
+}
---
+++ src/main/java/org/mbassy/subscription/FilteredSynchronousSubscription.java
@@ -0,0 +1,28 @@
+package org.mbassy.subscription;
+
+import org.mbassy.IMessageBus;
+import org.mbassy.IPublicationErrorHandler;
+import org.mbassy.listener.MessageFilter;
+import org.mbassy.listener.MessageHandlerMetadata;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+/**
+* Created with IntelliJ IDEA.
+* User: benni
+* Date: 11/14/12
+* Time: 3:49 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class FilteredSynchronousSubscription extends FilteredSubscription {
+
+
+    public FilteredSynchronousSubscription(IMessageBus mBassador, MessageHandlerMetadata messageHandler) {
+        super(mBassador, messageHandler);
+    }
+
+    protected void dispatch(final Object message, final Object listener){
+        invokeHandler(message, listener);
+    }
+}
